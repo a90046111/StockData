12b88ba7078a3c402b8c715d414f3fa27c853704
zb06 <zbdigital006@gmail.com>
2025-03-04 16:36:19 +0800

    add try except

--- project/DataProvider.py
+++ project/DataProvider.py
@@ -10,6 +10,9 @@ class DataProvider(object):
         y, m, d = date.split("-")
         date_config = {"y": y, "m": m, "d": d}
         for stock_type, url_config in self.settings.items():
-            url = url_config.format(**date_config)
-            stock_api_data = requests.get(url).json()
-            yield stock_type, stock_api_data
+            try:
+                url = url_config.format(**date_config)
+                stock_api_data = requests.get(url).json()
+                yield stock_type, stock_api_data
+            except:
+                self.send_msg(msg=f"stock_type: {stock_type}")
